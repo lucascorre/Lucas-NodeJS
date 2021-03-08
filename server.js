334505f8d7baa4fca5c2592b02edebf244cd59b8
Lucas <lucas.corre@ynov.com>
2021-03-08 17:05:17 +0100

    fin de cours du 08/03/2021

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-app.get('/', (req,res) => {
-    res.end('Hello World');
-});
+const chatFile =fs.readFileSync('./index.html')
+
+function chatController (req, res)  {
+    res.end(chatFile);
+}
+
+app.get('/', (req,res) => res.end('Hello World'));
+app.get('/chat', chatController);
 
 app.listen(3000, () => console.log('Server ready'));
